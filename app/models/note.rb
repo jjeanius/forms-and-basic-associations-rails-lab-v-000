7a4d2057a45efab603021788d82035bd0354c123
Judy McGoldrick <jjeanius@verizon.net>
2019-01-19 19:31:32 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,16 @@
 class Note < ActiveRecord::Base
   belongs_to :song
+
+  def note_contents=(name)
+    self.note_contents = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    self.note_contents ? self.note_contents.name : nil
+  end
+
+
+
+
+  
 end
